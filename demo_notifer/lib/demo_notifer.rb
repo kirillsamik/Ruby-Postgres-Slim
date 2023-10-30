5d6aa1ef03ce9ab8f85229d7b0d4daf4e34d0ef2
Кирилл Самарский <146956675+kirillsamik@users.noreply.github.com>
2023-10-30 16:06:11 +0300

    Add files via upload

---
+++ demo_notifer/lib/demo_notifer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "demo_notifer/version"
+
+module DemoNotifer
+  class Error < StandardError; end
+  # Your code goes here...
+  puts("Hello world!")
+end
